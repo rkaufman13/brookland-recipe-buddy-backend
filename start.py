0ada581594308e77b18f0c80fffba2382687e9d8
rachel <16939591+rkaufman13@users.noreply.github.com>
2025-09-20 19:04:15 -0400

    add request back in 422

--- start.py
+++ start.py
@@ -32,7 +32,7 @@ app = FastAPI()
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body, "request": request}),
     )
 
 
